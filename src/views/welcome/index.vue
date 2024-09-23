76dcdf9b388a756317be6e1b6a210f12c9a665aa
dannill <liudan0510@qq.com>
2024-09-23 15:04:32 +0800

    test: 测试不同项目之间串联的可能性
    前端项目打包采用/{项目名称}@{用户名}/的方式 这样不同项目即可实现串联

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -5,8 +5,7 @@ defineOptions({
 </script>
 
 <template>
-  <h1>Pure-Admin-Thin（国际化版本）</h1>
-  <h1>Pure-Admin-Thin（国际化版本）</h1>
-  <h1>Pure-Admin-Thin（国际化版本）</h1>
-  <h1>Pure-Admin-Thin（国际化版本）</h1>
+  <h1>Pure-Admin-Thin（国际化版本） demo@dannill</h1>
+  <h1><a href="/admin@dannill/#/welcome">admin@dannill</a></h1>
+  <h1><a href="/demo@dannill/#/welcome">demo@dannill</a></h1>
 </template>
